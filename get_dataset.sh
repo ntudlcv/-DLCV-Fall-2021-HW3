9a06a7d804da9f226bb089f77a3acdea6ecd43f0
ntudlcv <72860306+ntudlcv@users.noreply.github.com>
2021-11-20 23:01:59 +0800

    Update get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,5 +1,5 @@
 # Download dataset from Google drive
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1XQmblY4SDwrt6p8MT1YUKg21ertt0J5x' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1XQmblY4SDwrt6p8MT1YUKg21ertt0J5x" -O hw3_data.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1PDlObdTW6eLJiencXM5OdkSTFVSNvoOl' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1PDlObdTW6eLJiencXM5OdkSTFVSNvoOl" -O hw3_data.zip && rm -rf /tmp/cookies.txt
 
 # Unzip the downloaded zip file
 unzip ./hw3_data.zip
